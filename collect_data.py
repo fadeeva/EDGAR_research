012e61f3cf8894db1b8c18176277d9083849e6f5
Katya Fadeeva <katfadmail@gmail.com>
2025-07-13 22:20:54 +0200

    work only wo/ headless regime

--- collect_data.py
+++ collect_data.py
@@ -1,8 +1,20 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
 op = webdriver.ChromeOptions()
-op.add_argument('headless')
+# op.add_argument('headless')
 driver = webdriver.Chrome(options=op)
 
+
+
 driver.get('https://www.sec.gov/edgar/search/#/category=custom&entityName=TSLA&forms=10-Q')
+
+time.sleep(3)
+
+a = driver.find_element(By.CSS_SELECTOR, 'td.filetype')
+print(a.text)
+
+driver.quit()
