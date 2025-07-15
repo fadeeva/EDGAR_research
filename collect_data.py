f93d0377929aa2ce68601d41719a5e8870c9cff7
Katya Fadeeva <katfadmail@gmail.com>
2025-07-15 21:48:37 +0200

    get report links from edgar

--- collect_data.py
+++ collect_data.py
@@ -1,20 +1,32 @@
 import time
 
+import pickle as pk
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
+
+with open('data/sp500tickers.pickle', 'rb') as f:
+    tickers = pk.load(f)
+
 op = webdriver.ChromeOptions()
-# op.add_argument('headless')
+# op.add_argument('headless') # Headers issue
 driver = webdriver.Chrome(options=op)
 
+reports = {}
+for ticker in tickers:
+    try:
+        driver.get(f'https://www.sec.gov/edgar/search/#/category=custom&entityName={ticker}&forms=10-Q')
+        time.sleep(2)
+        elems = driver.find_elements(By.CSS_SELECTOR, 'td.filetype>a')
+        links = [elem.get_attribute('href') for elem in elems]
 
-
-driver.get('https://www.sec.gov/edgar/search/#/category=custom&entityName=TSLA&forms=10-Q')
-
-time.sleep(3)
-
-a = driver.find_element(By.CSS_SELECTOR, 'td.filetype')
-print(a.text)
+        reports[ticker] = links
+    except:
+        continue
 
 driver.quit()
+
+with open('data/reports.pickle', 'wb') as f:
+    pickle.dump(reports, f, pk.HIGHEST_PROTOCOL)
